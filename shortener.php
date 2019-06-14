3fd056a9561f6330e9b2c7271533460ad7956694
Giulio De Matteis <giuliod35@gmail.com>
2019-06-14 21:16:01 +0200

    Update shortener.php

--- shortener.php
+++ shortener.php
@@ -1,2 +1,28 @@
+<?php
+$url = $_GET["url"];
+$service = $_GET["service"];
 
-test
+if($service == "googl"){
+  
+}
+
+if($service == "isgd"){
+  $shortener = "https://is.gd/create.php?format=simple&url=".$url;
+  $read = fopen($shortener, 'r');
+  while (!feof($read)) {
+  $shorturl = fgets($read); 
+}
+fclose($read);
+  echo $shorturl;
+}
+
+if($service == "vgd){
+  $shortener = "https://v.gd/create.php?format=simple&url=".$url;
+  $read = fopen($shortener, 'r');
+  while (!feof($read)) {
+  $shorturl = fgets($read); 
+}
+fclose($read);
+  echo $shorturl;
+}
+?>
