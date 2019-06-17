eca78fca38db6533f9f90907953a248fc3438028
Giulio De Matteis <giuliod35@gmail.com>
2019-06-17 08:59:28 +0200

    Add files via upload

---
+++ client/AnyShortener/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace AnyShortener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.url = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.service = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.shorturl = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(123, 12);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(146, 20);
+            this.url.TabIndex = 0;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(275, 25);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Shorten";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Url to shorten:";
+            // 
+            // service
+            // 
+            this.service.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.service.FormattingEnabled = true;
+            this.service.Items.AddRange(new object[] {
+            "is.gd",
+            "v.gd",
+            "rebrand.ly",
+            "dot.tk"});
+            this.service.Location = new System.Drawing.Point(123, 38);
+            this.service.Name = "service";
+            this.service.Size = new System.Drawing.Size(146, 21);
+            this.service.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Service:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.shorturl);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Location = new System.Drawing.Point(4, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(354, 73);
+            this.panel1.TabIndex = 10;
+            this.panel1.Visible = false;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(234, 25);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(109, 23);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "Shorten another url";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // shorturl
+            // 
+            this.shorturl.Location = new System.Drawing.Point(90, 27);
+            this.shorturl.Name = "shorturl";
+            this.shorturl.ReadOnly = true;
+            this.shorturl.Size = new System.Drawing.Size(130, 20);
+            this.shorturl.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Your url is:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Look up the project on";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(120, 79);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(38, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Github";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 101);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.service);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.url);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Any Shortener";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox service;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox shorturl;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ client/AnyShortener/Form1.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyShortener
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            url.Text = "http://";
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+  
+            Uri uri = null;
+            string urll = url.Text;
+            if (!Uri.TryCreate(urll, UriKind.Absolute, out uri) || null == uri)
+
+            {
+                MessageBox.Show("Check the url format. The url need to be like this: http://example.com", "Any Shortener - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                // Check if url and service box are blank
+                if (url.Text == "")
+                {
+                    MessageBox.Show("You need to type an url.", "Any Shortener - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                if (service.Text == "")
+                {
+                    MessageBox.Show("You need to select a shortening service.", "Any Shortener - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    try
+                    {
+                        // Shortener
+                        if (service.Text == "is.gd")
+                        {
+
+                            WebClient myWebClient = new WebClient();
+                            string shortener = myWebClient.DownloadString("https://is.gd/create.php?format=simple&url=" + url.Text);
+
+                            shorturl.Text = shortener;
+                            panel1.Visible = true;
+                        }
+
+                        if (service.Text == "v.gd")
+                        {
+
+                            WebClient myWebClient = new WebClient();
+                            string shortener = myWebClient.DownloadString("https://v.gd/create.php?format=simple&url=" + url.Text);
+
+                            shorturl.Text = shortener;
+                            panel1.Visible = true;
+                        }
+
+
+                        if (service.Text == "rebrand.ly")
+                        {
+                            WebClient myWebClient = new WebClient();
+                            string shortener = myWebClient.DownloadString("https://code.giulio.top/dev/anyshortener/rebrandly.php?url=" + url.Text);
+
+                            shorturl.Text = shortener;
+                            panel1.Visible = true;
+                        }
+
+
+                        if (service.Text == "dot.tk")
+                        {
+                            WebClient myWebClient = new WebClient();
+                            string shortener = myWebClient.DownloadString("http://api.dot.tk/tweak/shorten?long=" + url.Text);
+                            url.Text = shortener;
+                            shorturl.Text = url.Lines[0];
+                            panel1.Visible = true;
+                        }
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("There was an error with your request.", "Any Shortener - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+            }
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            url.Clear();
+        }
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/themagiulio/AnyShortener/");
+        }
+
+    }
+}
